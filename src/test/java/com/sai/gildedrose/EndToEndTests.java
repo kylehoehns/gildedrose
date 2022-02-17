45290ca2980319cd40fad363baa9a273ca322cd2
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2022-02-17 12:41:45 -0600

    Build with GitHub actions

--- src/test/java/com/sai/gildedrose/EndToEndTests.java
+++ src/test/java/com/sai/gildedrose/EndToEndTests.java
@@ -3,6 +3,7 @@ package com.sai.gildedrose;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 @Transactional
 class EndToEndTests {
 
