cc0cf78c71dd5def007b59f7c3e76253070a72ca
Kyle Hoehns <9507268+kylehoehns@users.noreply.github.com>
2023-01-05 16:25:43 -0600

    chore: upgrade to Java 19 and Spring Boot 3

--- src/test/java/com/sai/gildedrose/EndToEndTests.java
+++ src/test/java/com/sai/gildedrose/EndToEndTests.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.hasSize;
